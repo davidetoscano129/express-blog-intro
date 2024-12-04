cc5ea2b6db097da5b80979bebf6179aeecd22a7c
davidetoscano129 <toscano.davidew@gmail.com>
2024-12-04 18:44:53 +0100

    feat: public images

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ app.use(express.static("public"));
 // Rotta di default
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
-})
+});
 
 // Rotta "/bacheca"
 app.get(`/bacheca`, (req, res) => {
     res.json({
         posts: post.data,
         length: post.data.length,
-    })
+    });
 
-})
+});
 
 // Avvia il server e logga che è in ascolto
 app.listen(port, () => {
--- data.js
+++ data.js
@@ -2,31 +2,31 @@ const data = [
     {
         "title": "Viaggio in Giappone",
         "content": "Un'esperienza indimenticabile tra i templi di Kyoto e i grattacieli di Tokyo.",
-        "image": "./images/pane_fritto_dolce",
+        "image": "images/ciambellone.jpeg",
         "tags": ["viaggi", "Giappone", "cultura", "avventura"]
     },
     {
         "title": "Ricetta della Torta al Cioccolato",
         "content": "Scopri come preparare una torta al cioccolato soffice e deliziosa in pochi semplici passi.",
-        "image": "https://example.com/torta-cioccolato.jpg",
+        "image": "images/cracker_barbabietola.jpeg",
         "tags": ["cucina", "dolci", "cioccolato", "ricette"]
     },
     {
         "title": "Guida alla Programmazione in Python",
         "content": "Tutto quello che devi sapere per iniziare a programmare con Python.",
-        "image": "https://example.com/python-guide.jpg",
+        "image": "images/pane_fritto_dolce.jpeg",
         "tags": ["programmazione", "Python", "tutorial", "tecnologia"]
     },
     {
         "title": "Le Migliori Serie TV del 2024",
         "content": "Una selezione delle serie TV più attese e amate del 2024.",
-        "image": "https://example.com/serie-tv-2024.jpg",
+        "image": "images/pasta_barbabietola.jpeg",
         "tags": ["intrattenimento", "serie TV", "recensioni", "2024"]
     },
     {
         "title": "Come Mantenersi in Forma con lo Yoga",
         "content": "I benefici dello yoga per il corpo e la mente, con esercizi adatti a tutti.",
-        "image": "https://example.com/yoga-benessere.jpg",
+        "image": "images/torta_paesana.jpeg",
         "tags": ["benessere", "yoga", "fitness", "salute"]
     }
 ]
